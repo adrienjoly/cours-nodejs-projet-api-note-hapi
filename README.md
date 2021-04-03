d046fe5089a5abcc187258687f1e294c7bf3f52a
Ludovic FAVIER <lfavier@live.fr>
2021-04-03 21:39:17 +0200

    implement jsonwebtoken, nodemon & hapi/boom. Beginning of authentication with JWT. TODO jwt.verif (doesn't work)

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Projet Node ESGI
 
 
 For local test use http://0.0.0.0:3000
+
+# authentication
+curl -X POST --header "Content-Type: application/json" --data "{\"username\":\"ludovic\"}" "http://localhost:3000/notes"
+
+# access notes
+curl -X GET --header "Content-Type: application/json" "http://localhost:3000/notes"
+
+## Generate key for JWT
+    ->open terminal and make command "node". 
+    ->Then in the shell node, make the command "require('crypto').randomBytes(64).toString('hex')" and obtain the key.
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+
 /* SERVEUR HTTP  */
 const Hapi = require('@hapi/hapi');
+
+const Boom = require('@hapi/boom');
+
+const jwt = require('jsonwebtoken');
 
 const init = async () => {
     const PORT = process.env.PORT || 3000;
@@ -9,14 +15,73 @@ const init = async () => {
         host: '0.0.0.0'
     });
 
+    const handleAuthenticateToken = async function (request, h) {
+        const authHeader = request.headers.authorization;
+        const token = authHeader && authHeader.split(' ')[1];
+        if (token == null) {
+            throw Boom.badRequest('Token not found');
+        }
+        jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+            if(err){
+                throw Boom.badRequest('Token invalid');
+            }
+            console.log("here");
+            return user;
+        });
+    };
+
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/notes',
+        options: {
+            pre: [
+                { method: handleAuthenticateToken, assign: 'auth', failAction: 'log'}
+            ]
+        },
         handler: (request, h) => {
-
-            return 'Hello World!';
+            var response = {};
+            //console.log(request.pre.auth.output.payload.error);
+            if(request.pre.auth.output.payload.error){
+                response.error = "Utilisateur non connecté";
+                response.notes = [];
+                return h.response(response).code(401);
+            }
+            else{
+                response.error = null;
+                response.notes = ['note 1', 'note 2'];
+                return h.response(response).code(200);
+            }
         }
     });
+
+    server.route({
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            var response = {};
+            if(!request.payload){
+                response.error = 'No username given';
+                response.accessToken = null;
+                return h.response(response).code(401);
+            }
+            const username = request.payload.username;
+            const user = { name: username }
+
+            const accessToken = jwt.sign(user, process.env.JWT_KEY, { expiresIn: "60"});
+            response.error = null;
+            response.accessToken = accessToken;
+            return h.response(response).code(200);
+
+        }
+    });
+    // server.route({
+    //     method: 'GET',
+    //     path: '/hello/{name}',
+    //     handler: (request, h) => {
+    //         const name = request.params.name;
+    //         return 'Hello ' + name;
+    //     }
+    // });
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
