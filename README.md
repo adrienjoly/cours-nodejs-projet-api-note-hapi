87d7faf46472ed1bfa3aa1482844cb3cf6bc25ec
Ludovic FAVIER <lfavier@live.fr>
2021-04-05 14:57:52 +0200

    beginning of get/put but issue with auhentication mongodb

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ curl -X POST --header "Content-Type: application/json" --data "{\"username\":\"l
 # access notes
 curl -X GET --header "Content-Type: application/json" "http://localhost:3000/notes"
 
-## Generate key for JWT
+# Generate key for JWT
     ->open terminal and make command "node". 
     ->Then in the shell node, make the command "require('crypto').randomBytes(64).toString('hex')" and obtain the key.
+
+# test to generate id with ObjectId()
+606b0233b814fd06e19e35a1
--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const Boom = require('@hapi/boom');
 
 const jwt = require('jsonwebtoken');
 
+/* SERVEUR MONGODB */
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('bson');
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'notes-api';
+
 const init = async () => {
+    await client.connect();
+
     const PORT = process.env.PORT || 3000;
 
     const server = Hapi.server({
@@ -20,10 +30,12 @@ const init = async () => {
         const authHeader = request.headers['x-access-token'];
         const token = authHeader && authHeader.split(' ')[1];
 
+        //have we found a token?
         if (token == null) {
             throw Boom.badRequest('Token not found');
         }
         try{
+            //is the token still valid?
             const user = jwt.verify(token, process.env.JWT_KEY);
             return user;
         } catch(err){
@@ -39,19 +51,70 @@ const init = async () => {
                 { method: handleAuthenticateToken, assign: 'auth', failAction: 'log'}
             ]
         },
-        handler: (request, h) => {
+        handler: async (request, h) => {
             var response = {};
+            response.error = null;
+            response.note = [];
+            var userId = "606b0233b814fd06e19e35a1"; //l'id user de mongodb. Variable temporaire.
+
+            //the user is not connected
             if(request.pre.auth.output){
-                //response.error = request.pre.auth.output.payload;
-                response.error = "Utilisateur non connecté"; //TODO
-                response.notes = [];
+                response.error = "Utilisateur non connecté";
                 return h.response(response).code(401);
             }
-            else{
-                response.error = null;
-                //response.user = request.pre.auth.name;
-                response.notes = ['note 1', 'note 2'];
+            try{
+                const collectionNotes = client.db(dbName).collection('notes');
+                const docs = await collectionNotes.find({_id: { $eq: userId}}).toArray();
+                response.note = docs;
                 return h.response(response).code(200);
+            }catch(err){
+                //response.error = "Error in Database"; //TODO
+                response.error = err;
+                return h.response(response).code(401);
+            }
+        }
+    });
+
+    server.route({
+        method: 'PUT',
+        path: '/notes',
+        options: {
+            pre: [
+                { method: handleAuthenticateToken, assign: 'auth', failAction: 'log'}
+            ]
+        },
+        handler: async (request, h) => {
+            var response = {};
+            response.error = null;
+            response.note = {};
+            var userId = "606b0233b814fd06e19e35a1"; //l'id user de mongodb. Variable temporaire.
+            console.log(userId);
+
+            //the user is not connected
+            if(request.pre.auth.output){
+                response.error = "Utilisateur non connecté";
+                return h.response(response).code(401);
+            }
+            //the user didn't give a content in the body request
+            if(!request.payload && !request.payload.content){
+                response.error = "Pas de content fourni";
+                return h.response(response).code(401);
+            }
+            try{
+                const collectionNotes = client.db(dbName).collection('notes');
+                await collectionNotes.insertOne({
+                    userId: userId, //TODO trouver à quel moment recuperer l'id d'utilisateur??
+                    content: request.payload.content,
+                    createdAt: Date().now(),
+                    lastUpdatedAt: null
+                });
+                const docs = await collectionNotes.find({_id: { $eq: userId}}).toArray();
+                response.note = docs;
+                return h.response(response).code(200);
+            }catch(err){
+                //response.error = "Error in Database"; //TODO
+                response.error = err;
+                return h.response(response).code(401);
             }
         }
     });
@@ -69,9 +132,10 @@ const init = async () => {
             const username = request.payload.username;
             const user = { name: username }
 
-            const accessToken = jwt.sign(user, process.env.JWT_KEY, { expiresIn: "1m"});
+            const accessToken = jwt.sign(user, process.env.JWT_KEY, { expiresIn: "2m"});
             response.error = null;
             response.accessToken = accessToken;
+            //TODO: connection mongodb et récupération de l'id de l'utilisateur connecté
             return h.response(response).code(200);
 
         }
